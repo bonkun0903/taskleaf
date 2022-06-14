42418a2d9a2ec4af4c5ada0bfb8ad3f2c13e63dc
bonkun0903 <bonkun0903@gmail.com>
2022-06-14 12:48:13 +0900

    バリデーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_225509) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_033736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
